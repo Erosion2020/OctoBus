4a7419f690935871412c4637cdb7438ec946f854
tongzhouli <tongzhou.li@chaitin.com>
2026-08-13 16:39:55 +0800

    Refine Zhizhangyi MBS numeric and fetch handling (#459)
    * Add Zhizhangyi MBS service adapter
    
    Signed-off-by: tongzhou.li <tongzhou.li@chaitin.com>
    
    * Align MBS service package metadata
    
    Signed-off-by: tongzhou.li <tongzhou.li@chaitin.com>
    
    * Validate MBS user id mutations
    
    Signed-off-by: tongzhou.li <tongzhou.li@chaitin.com>
    
    * Refine MBS mutation request signing
    
    Signed-off-by: tongzhou.li <tongzhou.li@chaitin.com>
    
    * Require MBS condition mutations filters
    
    Signed-off-by: tongzhou.li <tongzhou.li@chaitin.com>
    
    * Cover remaining MBS handlers
    
    Signed-off-by: tongzhou.li <tongzhou.li@chaitin.com>
    
    * Align MBS batch mutation signatures
    
    Signed-off-by: tongzhou.li <tongzhou.li@chaitin.com>
    
    * Harden MBS fetch and type validation
    
    Signed-off-by: tongzhou.li <tongzhou.li@chaitin.com>
    
    * Refine Zhizhangyi MBS numeric and fetch handling
    
    * fix: register service in package and dispatcher
    
    * test: cover MBS error and dispatch paths
    
    ---------
    
    Signed-off-by: tongzhou.li <tongzhou.li@chaitin.com>
    Co-authored-by: kingfs <zhenzhen086@126.com>

--- services/bin/octobus-tentacles.js
+++ services/bin/octobus-tentacles.js
@@ -308,6 +308,10 @@ const services = {
     entryFile: "../filigran__opencti/bin/opencti.js",
     serviceModule: "../filigran__opencti/src/service.js",
   },
+  "zhizhangyi-mbs": {
+    entryFile: "../zhizhangyi__mbs/bin/zhizhangyi-mbs.js",
+    serviceModule: "../zhizhangyi__mbs/src/service.js",
+  },
   "qianxin-caasm": {
     entryFile: "../qianxin__caasm_v1/bin/qianxin-caasm.js",
     serviceModule: "../qianxin__caasm_v1/src/service.js",
